dd4dffd73d6f2b35868a33f159c7f00f35c64973
Ztechboss <trevmg7@gmail.com>
2018-03-22 20:24:37 -0700

    Created homepage

--- routes.js
+++ routes.js
@@ -1,7 +1,5 @@
 module.exports = function(app) {
-	app.use('/static', express.static('./static'));
-	app.set('views',__dirname + '/views');
-	app.set('view engine', 'html');
+	
 
 	app.get('/', function(req, res) {
 		res.render('home.html');
