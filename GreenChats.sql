1933c76775833135d71c7bd8a3c5c96f6fb0ace2
Ztechboss <trevmg7@gmail.com>
2018-03-25 22:13:04 -0700

    Updated database schema

--- GreenChats.sql
+++ GreenChats.sql
@@ -1,7 +1,15 @@
+drop table if exists Posts;
 drop table if exists Accounts;
 
 create table Accounts (
+	acctID INT PRIMARY KEY AUTO_INCREMENT,
 	firstname VARCHAR(20),
 	lastname VARCHAR(30),
 	email VARCHAR(30)
+);
+
+create table Posts (
+	postID INT PRIMARY KEY AUTO_INCREMENT,
+	content TEXT,
+	acctID INT FOREIGN KEY REFERENCES Accounts (acctID)
 );
