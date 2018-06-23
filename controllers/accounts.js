85ba97f361833ffffedeeb633d6c0b7de59b1c0c
tmgreenside <trevmg7@gmail.com>
2018-06-22 22:03:23 -0500

    Altered database functonality

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -1,14 +1,44 @@
-var db = require('./controllers/database');
+var pool = require('./database')
 
 exports.signin = function (req, res) {
-    if (!req.body.enterEmail || !req.body.enterPass) {
+    if (req.body.enterEmail === "" || req.body.enterPass === "") {
         res.render('/', {message: "Invalid email or password"});
     } else {
-        result = db.login();
-        res.send(result);
+        userenter = req.body.enterEmail;
+        hashed_pw = req.body.enterPass;
+        statement = "select * from Accounts where email = ? and password = ?";
+        pool.query(statement, [userenter, hashed_pw], function (err, result) {
+            if (err || result.length === 0)
+                return "";
+            else {
+                return result;
+            }
+        });
     }
 };
 
 exports.signup = function(req, res) {
-    
-}
+    pass1 = req.body.pass1;
+    pass2 = req.body.pass2;
+    if (pass1 !== pass2){
+        console.log("Passwords don't match");
+        res.render('signup.html', {message: "Passwords must match"});
+    } else {
+        n = {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            pass: req.body.pass1
+        };
+        stmt = "insert into Accounts (firstname, lastname, email, password) values (?, ?, ?, ?)";
+        connection.query(stmt, [n.firstname, n.lastname, n.email, n.pass], function(err, result) {
+            if (err) {
+                console.log("DB failed");
+                console.log(err);
+                return false;
+            }
+            return true;
+        });
+        
+    }
+};
--- controllers/database.js
+++ controllers/database.js
@@ -1,25 +1,23 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
     host: 'localhost',
     user: 'trevapp',
     password: 'bowers321',
     database: 'GreenChats'
 });
 
-function login(userenter, hashed_pw) {
-    statement = "select * from Accounts where email = ? and password = ?";
-    connection.query(statement, [userenter, hashed_pw], function (err, result) {
-        if (err || result.length === 0)
-            return "";
-        else {
-            return result;
-        }
-    });
-}
+// Attempt to catch disconnects 
+pool.on('connection', function (connection) {
+  console.log('DB Connection established');
+
+  connection.on('error', function (err) {
+    console.error(new Date(), 'MySQL error', err.code);
+  });
+  connection.on('close', function (err) {
+    console.error(new Date(), 'MySQL close', err);
+  });
+
+});
+
+module.exports = pool;
