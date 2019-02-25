9d73da5c1c5e14f2375e07e2d5efd6c12854def2
tmgreenside <trevmg7@gmail.com>
2019-02-24 17:31:15 -0800

    Untracking old files, removing from repo

--- testData.sql
+++
@@ -1,8 +0,0 @@
-use GreenChats;
--- 
--- insert into Accounts (firstname, lastname, email, birthdate, password, occupation) values ("Alyssa", "Greenside", "alyssacink@gmail.com", STR_TO_DATE('04/06/2018', '%m/%d/%Y'), "pass", "Book Publisher");
-
-select * from Accounts where acctID = 10001;
-
-
-
