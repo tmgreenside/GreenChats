604c90bb1f567fc61dfbd1bced8a10b459624912
tmgreenside <trevmg7@gmail.com>
2019-03-01 12:00:08 -0800

    Working on cookies and sessions

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
 var app = express();
 
 var registration = require('./Registration/registration.js');
@@ -6,8 +9,8 @@ var registration = require('./Registration/registration.js');
 app.set('view engine', 'ejs');
 app.set('views', './views')
 
-
-app.use('/Register', registration);
+app.use(cookieParser());
+app.use('/register', registration);
 
 app.get('/', function(req, res) {
     res.render("index");
