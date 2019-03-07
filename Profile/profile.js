a4294713f217862f89c695cccce0ee005db00568
tmgreenside <trevmg7@gmail.com>
2019-03-07 00:38:59 -0800

    Working on user authentication

--- Profile/profile.js
+++ Profile/profile.js
@@ -1,3 +1,17 @@
-export.login = function(req, res) {
-    res.send("Does post stuff");
+var pool = require('../Database/db');
+
+exports.login = function(req, res) {
+    var emailEntry = req.body.email;
+    var passEntry = req.body.pass;
+    var query = "SELECT id, firstname, middlename, lastname, email FROM Users WHERE email = ? AND password = ?";
+    pool.query(query, [emailEntry, passEntry], function(err, result) {
+        if (err) {
+            console.log(err);
+            res.send({response: "failure"});
+        }
+        else {
+            console.log(result[0]);
+            res.send({response: "success"});
+        }
+    });
 }
