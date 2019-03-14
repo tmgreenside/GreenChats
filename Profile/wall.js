295a0b4ba6e5769fe3fe069b0c2c8aebee5026e0
tmgreenside <trevmg7@gmail.com>
2019-03-14 11:07:29 -0700

    Working on wall

--- Profile/wall.js
+++ Profile/wall.js
@@ -10,7 +10,7 @@ function getUserContext(req) {
 
 exports.displayNewsFeed = function(req, res) {
     var context = getUserContext(req);
-    queryPosts = "SELECT * FROM Posts WHERE Profile IN (SELECT profile1 FROM Friendships WHERE profile2 = ?)";
+    queryPosts = "SELECT u.firstname, u.lastname, u.id, p.* FROM Posts as p, Users as u WHERE Profile IN (SELECT profile1 FROM Friendships WHERE profile2 = ?)";
     mediaPosts = "SELECT P.postID, P.type, P.filePath FROM PostMedia as M, Posts as P WHERE P.id = M.PostID AND P.profile IN (SELECT profile2 FROM Friendships WHERE profile1 = ?)";
     pool.query(queryPosts, [req.session.user.id], function(err, result) {
         if (err) {
@@ -18,9 +18,8 @@ exports.displayNewsFeed = function(req, res) {
         }
         else {
             pool.query(mediaPosts, [req.session.user.id], function(err, result2) {
-                res.send(result);
+                res.render('profile/wall', context);
             });
         }
     });
-    res.render("profile/wall", context);
 }
