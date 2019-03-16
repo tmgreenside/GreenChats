410eac859149b26fcba4cabaf74c53b8c59f332b
tmgreenside <trevmg7@gmail.com>
2019-03-16 11:28:08 -0700

    Replaced default session storage with express-mysql-session.

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var mysql = require('mysql');
+var mySQLStore = require('express-mysql-session')(session);
 var bodyParser = require('body-parser');
 
 var app = express();
 
+var credential = require('./Database/credentials.js');
+
 var registration = require('./Registration/registration.js');
 var account = require('./Accounts/accounts.js');
 var main = require('./Profile/main.js');
+
+var sessionStore = new mySQLStore(credential.db);
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.engine('ejs', require('express-ejs-extend'));
@@ -17,7 +22,9 @@ app.set('views', './views');
 app.use(express.static('static'));
 app.use(cookieParser());
 app.use(session({
+    key: "greenchats_cookie",
     secret: "lucky_cat",
+    store: sessionStore,
     resave: false,
     saveUninitialized: false
  }));
