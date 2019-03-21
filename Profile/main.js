e7e4ac033397011db5b4ffdb10b0116ffd1d8a50
tmgreenside <trevmg7@gmail.com>
2019-03-21 00:25:43 -0700

    Working on viewing profiles

--- Profile/main.js
+++ Profile/main.js
@@ -1,10 +1,29 @@
 var pool = require('../Database/db');
 
-exports.getHomePage = function(req, res) {
+function showProfile(profileID, req, res) {
     var dataQuery = "SELECT * FROM Users WHERE ID = ?";
-    pool.query(dataQuery, [req.session.user.id], function(err, result) {
+    pool.query(dataQuery, [profileID], function(err, result) {
         var context = {user: result[0]};
         console.log(context);
         res.render('profile/profile', context);
     });
+}
+
+/*
+This function calls the showProfile() function so that users can view their
+own profile.
+*/
+exports.getHomePage = function(req, res) {
+    showProfile(req.session.user.id, req, res);
+}
+
+/*
+This function uses the showProfile() function to show another user's
+profile.
+
+TO DO: add privacy functionality so that some users can see the whole
+profile while others (i.e. non-friends) cannot.
+*/
+exports.viewProfile = function(req, res) {
+
 }
