140174662698f5f7ac1de8996ec4b157aab7d3f7
tmgreenside <trevmg7@gmail.com>
2019-03-21 00:16:24 -0700

    Making session data more efficient

--- Profile/login.js
+++ Profile/login.js
@@ -3,7 +3,7 @@ var pool = require('../Database/db');
 exports.login = function(req, res) {
     var emailEntry = req.body.emailEntry;
     var passEntry = req.body.passEntry;
-    var query = "SELECT * FROM Users WHERE email = ? AND password = ?";
+    var query = "SELECT firstname, lastname, id FROM Users WHERE email = ? AND password = ?";
     pool.query(query, [emailEntry, passEntry], function(err, result) {
         if (err) {
             console.log(err);
@@ -14,12 +14,8 @@ exports.login = function(req, res) {
         }
         else {
             req.session.user = {
-                email: result[0]['email'],
                 first: result[0]['firstname'],
-                middle: result[0]['middlename'],
                 last: result[0]['lastname'],
-                gender: result[0]['gender'],
-                birthdate: reqult[0]['birthdate'],
                 id: result[0]['id']
             }
             res.redirect('/');
--- Profile/main.js
+++ Profile/main.js
@@ -1,7 +1,10 @@
 var pool = require('../Database/db');
 
 exports.getHomePage = function(req, res) {
-    var context = {user: req.session.user};
-    console.log(context);
-    res.render('profile/profile', context);
+    var dataQuery = "SELECT * FROM Users WHERE ID = ?";
+    pool.query(dataQuery, [req.session.user.id], function(err, result) {
+        var context = {user: result[0]};
+        console.log(context);
+        res.render('profile/profile', context);
+    });
 }
