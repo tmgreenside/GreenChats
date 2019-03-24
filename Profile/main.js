e5c797f3df48256abc234bf9f812885b5f3a3112
tmgreenside <trevmg7@gmail.com>
2019-03-23 17:39:33 -0700

    Working on post data with profiles

--- Profile/main.js
+++ Profile/main.js
@@ -2,10 +2,21 @@ var pool = require('../Database/db');
 
 function showProfile(profileID, req, res) {
     var dataQuery = "SELECT * FROM Users WHERE ID = ?";
+    var queryPosts = "SELECT u.firstname, u.lastname, u.id, p.* FROM Posts as p, Users as u WHERE Profile IN (SELECT profile1 FROM Friendships WHERE profile2 = ?) OR Profile = ?";
+    var mediaPosts = "SELECT P.postID, P.type, P.filePath FROM PostMedia as M, Posts as P WHERE P.id = M.PostID AND P.profile IN (SELECT profile2 FROM Friendships WHERE profile1 = ?) OR P.profile = ?";
     pool.query(dataQuery, [profileID], function(err, result) {
         var context = {user: result[0]};
-        console.log(context);
-        res.render('profile/profile', context);
+        pool.query(queryPosts, [profileID, profileID], function(err, result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                context.resultPosts = result;
+                pool.query(mediaPosts, [req.session.user.id, req.session.user.id], function(err, result2) {
+                    res.render('profile/profile', context);
+                });
+            }
+        });
     });
 }
 
--- Profile/profile.js
+++ Profile/profile.js
@@ -23,4 +23,6 @@ router.post('/wall', wall.postWall);
 
 router.get('/home', main.getHomePage);
 
+router.post('/searchFriends', search.search);
+
 module.exports = router;
