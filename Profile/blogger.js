1c4f0be46dd7d84732dc42be9f219276fa9a7e84
tmgreenside <trevmg7@gmail.com>
2019-03-27 23:06:34 -0700

    Working on file upload

--- Profile/blogger.js
+++ Profile/blogger.js
@@ -2,6 +2,19 @@ var multer  = require('multer');
 var fs = require('fs-extra');
 var pool = require('../Database/db');
 
+function moveFiles(req) {
+    var fileName;
+    var originalName;
+    var userDir = req.session.user.id;
+    for (var file in req.files) {
+        fileName = req.files[file].filename;
+        originalName = req.files[file].originalname;
+        fs.copy('../uploads/' + fileName, '../uploads/' + userDir + '/' + originalName).then(
+            () => console.log('success!')
+        ).catch(err => console.error(err)); // still in progress
+    }
+}
+
 exports.submitPost = function(req, res, source) {
     var postText = req.body.blog;
     var insertion = "INSERT INTO Posts (postText, profile) VALUES (?, ?)";
@@ -11,21 +24,7 @@ exports.submitPost = function(req, res, source) {
         }
         else {
             if (req.files) {
-                var fileName;
-                var originalName;
-                var userDir = req.session.user.id;
-                for (var file in req.files) {
-                    fileName = req.files[file].filename;
-                    originalName = req.files[file].originalname;
-                    fs.move('../uploads/' + fileName, '../uploads/' + userDir + '/' + originalName, function (err) {
-                        if (err) {
-                            return console.error(err);
-                        }
-                        else {
-                            console.log("Success");
-                        }
-                    });
-                }
+                setTimeout(moveFiles, 1000, req);
             }
 
             if (source === "home") {
