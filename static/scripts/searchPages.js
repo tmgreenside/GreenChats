f5021dd3d0387b5f045d2ab78ac54f6bac49f192
tmgreenside <trevmg7@gmail.com>
2019-03-31 23:36:58 -0700

    Working on search feature

--- static/scripts/searchPages.js
+++ static/scripts/searchPages.js
@@ -2,7 +2,15 @@ var progressTimeout = null;
 
 function submitSearchRequest() {
     var searchParams = $("#searchPages").val();
-    
+    $.ajax({
+		type: "POST",
+        data: {
+            'searchParams': searchParams
+        },
+        success: function(data) {
+            $('#searchResults').html(data);
+        }
+    });
 }
 
 $(document).ready(function() {
