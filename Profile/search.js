6f1b7068d7c9db0603c723c7d466008b4c1312e6
tmgreenside <trevmg7@gmail.com>
2019-04-03 00:33:46 -0700

    Working on search

--- Profile/search.js
+++ Profile/search.js
@@ -1,5 +1,7 @@
 var pool = require('../Database/db');
 
 exports.search = function(req, res) {
+    var params = req.body.searchParams.split(' ');
+    console.log(params);
     res.send("Work in progress");
 }
