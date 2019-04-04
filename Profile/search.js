fc8a4ddb3fe46ee37322d6b339b83671fc0316db
tmgreenside <trevmg7@gmail.com>
2019-04-03 22:29:54 -0700

    Working on friend search

--- Profile/search.js
+++ Profile/search.js
@@ -2,6 +2,14 @@ var pool = require('../Database/db');
 
 exports.search = function(req, res) {
     var params = req.body.searchParams.split(' ');
-    console.log(params);
+    var searchQuery = "SELECT firstname, lastname, id FROM Users WHERE firstname IN (" + params.join() + ")";
+    searchQuery += " OR lastname IN (" + params.join() + ")";
+    pool.query(searchQuery, function(err, result) {
+        if (result.length < 1) {
+            res.send("No results.");
+        } else {
+            res.send(result);
+        }
+    });
     res.send("Work in progress");
 }
