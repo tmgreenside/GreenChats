0e29afbf70ec7aa0c916d9fdd03f61fba0f086ca
tmgreenside <trevmg7@gmail.com>
2019-04-04 09:01:48 -0700

    Altered search query to limit number of results.

--- Profile/search.js
+++ Profile/search.js
@@ -1,10 +1,11 @@
 var pool = require('../Database/db');
 
+// TO DO: order by mutual friends or relevance
 exports.search = function(req, res) {
     var params = req.body.searchParams.split(' ');
     var searchQuery = "SELECT firstname, lastname, id FROM Users WHERE firstname IN ('" + params.join(',') + "')";
     searchQuery += " OR lastname IN ('" + params.join(',') + "')";
-    console.log(searchQuery);
+    searchQuery += " LIMIT 20";
     pool.query(searchQuery, function(err, result) {
         if (err) {
             res.send("No results.");
