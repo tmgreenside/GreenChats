a57e962c01e4aa496e14c37782dd588885bc5234
tmgreenside <trevmg7@gmail.com>
2019-04-26 22:30:12 -0700

    Fixing CSS for consistency between browsers

--- Database/db.js
+++ Database/db.js
@@ -10,15 +10,14 @@ var pool = mysql.createPool(login);
 
 // Attempt to catch disconnects
 pool.on('connection', function (connection) {
-  console.log('DB Connection established');
+    console.log('DB Connection established');
 
-  connection.on('error', function (err) {
-    console.error(new Date(), 'MySQL error', err.code);
-  });
-  connection.on('close', function (err) {
-    console.error(new Date(), 'MySQL close', err);
-  });
-
+    connection.on('error', function (err) {
+        console.error(new Date(), 'MySQL error', err.code);
+    });
+    connection.on('close', function (err) {
+        console.error(new Date(), 'MySQL close', err);
+    });
 });
 
 module.exports = pool;
